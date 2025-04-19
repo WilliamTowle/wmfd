ad4d54a9deaf56d91fda14c31a0a71ae0a6ebb34
William Towle <william_towle@yahoo.co.uk>
2025-04-19 14:47:11 +0100

    Tidy/simplify staging build

--- config/staging/wmfd-2o.mk
+++ config/staging/wmfd-2o.mk
@@ -5,32 +5,19 @@ include ${CONFIG_DIR}/staging.mk
 include ${CONFIG_DIR}/package/freedos-kernel.mk
 include ${CONFIG_DIR}/package/freedos-command.mk
 
+STAGING_PACKAGES=kernel command
 
 ## Rules
 
 .PHONY: download-staging-all
-download-staging-all: download-prepare
-ifneq (${KERNEL_VERSION},)
-	make download-staging-kernel
-endif
-ifneq (${COMMAND_VERSION},)
-	make download-staging-command
-endif
+download-staging-all: download-prepare \
+	$(patsubst %,download-staging-%,${STAGING_PACKAGES})
 
 .PHONY: verify-staging-all
-verify-staging-all: download-staging-all
-ifneq (${KERNEL_VERSION},)
-	make verify-staging-kernel
-endif
-ifneq (${COMMAND_VERSION},)
-	make verify-staging-command
-endif
+verify-staging-all: download-staging-all \
+	$(patsubst %,verify-staging-%,${STAGING_PACKAGES})
 
 .PHONY: build-staging
-build-staging: download-prepare staging-prepare build-toolchain | ${DOWNLOAD_DIR} ${STAGING_DIR}
-ifneq (${KERNEL_VERSION},)
-	make install-staging-kernel
-endif
-ifneq (${COMMAND_VERSION},)
-	make install-staging-command
-endif
+build-staging: download-prepare staging-prepare build-toolchain \
+	$(patsubst %,install-staging-%,${STAGING_PACKAGES}) \
+	| ${DOWNLOAD_DIR} ${STAGING_DIR}

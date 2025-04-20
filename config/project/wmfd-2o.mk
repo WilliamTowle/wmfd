cbe49b3730a5498b0af199d4743a615fb5bed393
William Towle <william_towle@yahoo.co.uk>
2025-04-20 18:24:37 +0100

    wmfd-2o: add TOOLCHAIN_WITH_SYSLINUX switch
    Enable a choice between sys-freedos and syslinux as the boot loader
    installer for floppy images.
    
    In the case of syslinux, installation to a boot sector requires
    root permissions regardless of whether the target is a device or an
    image. The implementation therefore retains sys-freedos as the
    toolchain's default installer unless TOOLCHAIN_WITH_SYSLINUX=y has
    been explicitly specified.

--- config/project/wmfd-2o.mk
+++ config/project/wmfd-2o.mk
@@ -1,8 +1,14 @@
 ## wmfd-de project configuration - wmfd-2o
 
+TOOLCHAIN_WITH_SYSLINUX?=n
+
 include ${CONFIG_DIR}/download.mk
 
+ifneq (${TOOLCHAIN_WITH_SYSLINUX},n)
+include ${CONFIG_DIR}/toolchain/freedos-syslinux.mk
+else
 include ${CONFIG_DIR}/toolchain/freedos-sys-freedos.mk
+endif
 include ${CONFIG_DIR}/staging/wmfd-2o.mk
 
 MEDIA_FILENAME=wmfd-2o.img
@@ -22,8 +28,14 @@ verify-all: verify-toolchain-all verify-staging-all
 .PHONY: deploy-media
 deploy-media: build-staging | ${STAGING_DIR}
 	mformat -C -f 1440 -i ${STAGING_DIR}/${MEDIA_FILENAME} ::
+ifneq (${TOOLCHAIN_WITH_SYSLINUX},n)
+	( mkdir -p ${STAGING_DIR}/rootfs/boot/syslinux && printf '%s\n' 'prompt 1' 'timeout 120' 'default dos' '' 'label dos' '    COM32 /boot/syslinux/chain.c32' '    APPEND freedos=/kernel.sys' > ${STAGING_DIR}/rootfs/boot/syslinux/syslinux.cfg )
+	sudo sh -c "PATH=${PATH} syslinux --install ${STAGING_DIR}/${MEDIA_FILENAME}"
+	mmd -i ${STAGING_DIR}/${MEDIA_FILENAME} ::/boot ::/boot/syslinux && mcopy -i ${STAGING_DIR}/${MEDIA_FILENAME} ${TOOLCHAIN_DIR}/lib/syslinux/chain.c32 ${STAGING_DIR}/rootfs/boot/syslinux/syslinux.cfg ::/boot/syslinux/
+else
 	sys-freedos.pl --disk=${STAGING_DIR}/${MEDIA_FILENAME} --offset=0 --drive=0
-	mcopy -i ${STAGING_DIR}/${MEDIA_FILENAME} ${STAGING_DIR}/rootfs/* ::
+endif
+	mcopy -i ${STAGING_DIR}/${MEDIA_FILENAME} ${STAGING_DIR}/rootfs/*.* ::
 
 .PHONY: clean
 clean: toolchain-clean staging-clean

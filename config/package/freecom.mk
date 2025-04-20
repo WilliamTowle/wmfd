3d00a5ddd204c8f10985a1c2486ed3b795d87427
William Towle <william_towle@yahoo.co.uk>
2025-04-20 17:36:29 +0100

    build: add freecom 'xmsswap' support
    Adjust download/build to use com082pl3-xmsswap.zip (i386+ version)

--- config/package/freecom.mk
+++ config/package/freecom.mk
@@ -1,7 +1,10 @@
 ## wmfd-de package configuration - FreeCOM (FreeDOS command shell)
 
+# SourceForge FreeDOS page recommends com082pl3-xmsswap.zip for 386+
+
 COMMAND_VERSION=082pl3
-COMMAND_ZIPFILE=${DOWNLOAD_DIR}/com082pl3.zip
+#|COMMAND_ZIPFILE=${DOWNLOAD_DIR}/com082pl3.zip
+COMMAND_ZIPFILE=${DOWNLOAD_DIR}/com082pl3-xmsswap.zip
 COMMAND_URL=https://sourceforge.net/projects/freedos/files/FreeCOM/082pl3%20%28use%20xmsswap%20for%20386%2B%20PC%29/$(notdir ${COMMAND_ZIPFILE})/download
 
 
@@ -13,15 +16,28 @@ download-staging-freecom:
 
 .PHONY: verify-staging-freecom
 verify-staging-freecom: download-staging-freecom
-	make download-verify FILE=${COMMAND_ZIPFILE} EXPECTED_CHECKSUM=0e2e501d1f9d8ffa9bdbbd08b019f2a4
+	case `basename ${COMMAND_ZIPFILE}` in \
+	com082pl3.zip) make download-verify FILE=${COMMAND_ZIPFILE} EXPECTED_CHECKSUM=0e2e501d1f9d8ffa9bdbbd08b019f2a4 ;; \
+	com082pl3-xmsswap.zip) make download-verify FILE=${COMMAND_ZIPFILE} EXPECTED_CHECKSUM=48cfb02bc87aa0e2deb73b8f5ae30653 ;; \
+	*) exit 1 ;; \
+	esac
 
 .PHONY: install-staging-freecom
 install-staging-freecom: verify-staging-freecom
 	[ -r ${STAGING_DIR}/rootfs/command.com ] || { \
 		mkdir -p ${STAGING_DIR}/temp && \
 		cd ${STAGING_DIR}/temp && \
-		unzip ${COMMAND_ZIPFILE} && \
-		unzip freecom.zip && \
-		cp freecom/command.com ${STAGING_DIR}/rootfs && \
+		case `basename ${COMMAND_ZIPFILE}` in \
+		com082pl3.zip) \
+			unzip ${COMMAND_ZIPFILE} && \
+			unzip freecom.zip && \
+			cp freecom/command.com ${STAGING_DIR}/rootfs \
+			;; \
+		com082pl3-xmsswap.zip) \
+			unzip ${COMMAND_ZIPFILE} && \
+			cp command.com ${STAGING_DIR}/rootfs \
+			;; \
+		*) exit 1 ;; \
+		esac && \
 		cd ${STAGING_DIR} && rm -rf ./temp ;\
 		}

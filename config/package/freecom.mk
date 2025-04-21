ffb2a55d3009a59077db915f0514fa02d91ed4f1
William Towle <william_towle@yahoo.co.uk>
2025-04-21 15:46:43 +0100

    package config: use 'freecom' name

--- config/package/freecom.mk
+++ config/package/freecom.mk
@@ -1,4 +1,4 @@
-## wmfd-de package configuration - freedos-command
+## wmfd-de package configuration - FreeCOM (FreeDOS command shell)
 
 COMMAND_VERSION=082pl3
 COMMAND_ZIPFILE=${DOWNLOAD_DIR}/com082pl3.zip
@@ -7,16 +7,16 @@ COMMAND_URL=https://sourceforge.net/projects/freedos/files/FreeCOM/082pl3%20%28u
 
 ## Rules:
 
-.PHONY: download-staging-command
-download-staging-command:
+.PHONY: download-staging-freecom
+download-staging-freecom:
 	make download-file DOWNLOAD_URL=${COMMAND_URL} DOWNLOAD_OUTFILE=${COMMAND_ZIPFILE}
 
-.PHONY: verify-staging-command
-verify-staging-command: download-staging-command
+.PHONY: verify-staging-freecom
+verify-staging-freecom: download-staging-freecom
 	make download-verify FILE=${COMMAND_ZIPFILE} EXPECTED_CHECKSUM=0e2e501d1f9d8ffa9bdbbd08b019f2a4
 
-.PHONY: install-staging-command
-install-staging-command: verify-staging-command
+.PHONY: install-staging-freecom
+install-staging-freecom: verify-staging-freecom
 	[ -r ${STAGING_DIR}/rootfs/command.com ] || { \
 		mkdir -p ${STAGING_DIR}/temp && \
 		cd ${STAGING_DIR}/temp && \

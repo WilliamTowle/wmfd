d576657086ab12405215bd6051481f48cfef733e
William Towle <william_towle@yahoo.co.uk>
2025-10-11 15:33:36 +0100

    build: add MEDIA_TYPE support
    Introduce a MEDIA_TYPE variable to control media generation when
    the 'deploy-media' target is invoked.
    
    Previous behaviour (creation of floppy-sized disk images) is
    retained through the default value of 'floppy', whereas 'zip' will
    generate Zip100 images instead.
    
    Output file names change according to media type so they can be
    distinguished.

--- config/media/floppy-sys-freedos.mk
+++ config/media/floppy-sys-freedos.mk
@@ -1,8 +1,10 @@
 ## sys-freeDOS floppy build
 
 
+ifeq (${MEDIA_TYPE},floppy)
 ${MEDIA_OUTFILE}: | ${STAGING_DIR}
 	mformat -C -f 1440 -i $@
+endif
 
 
 .PHONY: prepare-media-floppy
--- config/project/wmfd-2o.mk
+++ config/project/wmfd-2o.mk
@@ -11,12 +11,13 @@ include ${CONFIG_DIR}/toolchain/freedos-sys-freedos.mk
 endif
 include ${CONFIG_DIR}/staging/wmfd-2o.mk
 
-
-MEDIA_FILENAME=wmfd-2o.img
+MEDIA_TYPE=floppy
+MEDIA_FILENAME=wmfd-2o-${MEDIA_TYPE}.img
 MEDIA_OUTFILE=${STAGING_DIR}/${MEDIA_FILENAME}
 
 ifneq (${TOOLCHAIN_WITH_SYSLINUX},n)
 include ${CONFIG_DIR}/media/floppy-syslinux.mk
+include ${CONFIG_DIR}/media/zip-syslinux.mk
 else
 include ${CONFIG_DIR}/media/floppy-sys-freedos.mk
 endif
@@ -38,11 +39,15 @@ verify-all: verify-toolchain-all verify-staging-all
 .PHONY: deploy-media
 
 .PHONY: deploy-media-prepare
-deploy-media-prepare: build-toolchain prepare-media-floppy
+deploy-media-prepare: build-toolchain prepare-media-${MEDIA_TYPE}
 
 .PHONY: deploy-media-install
 deploy-media-install: deploy-media-prepare build-staging
+ifneq (${MEDIA_TYPE},floppy)
+	MTOOLSRC=${MEDIA_MTOOLSRC} mcopy ${STAGING_DIR}/rootfs/*.* z:
+else
 	mcopy -i ${MEDIA_OUTFILE} ${STAGING_DIR}/rootfs/*.* ::
+endif
 
 
 deploy-media: deploy-media-prepare deploy-media-install
